0e862b7c99513ac6b7969edd266d79e20f405d64
userz <sgoodman@aiddata.org>
2017-10-06 15:25:34 -0400

    Fix tests again

--- tests/test_zonal.py
+++ tests/test_zonal.py
@@ -621,8 +621,8 @@ def test_geom_split_main():
 def test_geom_split_categorical():
     polygons = os.path.join(DATA, 'polygons.shp')
     categorical_raster = os.path.join(DATA, 'slope_classes.tif')
-    stats1 = zonal_stats(polygons, categorical_raster, categorical=True)
-    stats2 = zonal_stats(polygons, categorical_raster, percent_cover_weighting=True, categorical=True, limit=50)
+    stats1 = zonal_stats(polygons, categorical_raster, categorical=True, all_touched=True)
+    stats2 = zonal_stats(polygons, categorical_raster, percent_cover_weighting=True, categorical=True, limit=50, all_touched=True)
     assert len(stats1) == len(stats2) == 2
     assert stats1[0][1.0] == stats2[0][1.0]  == 95
     assert 5.0 in stats1[1] and 5.0 in stats2[1]
